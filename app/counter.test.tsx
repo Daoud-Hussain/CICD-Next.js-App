2cebe79afb7c29d8177a1902b01fdefecbd51db8
Daoud-Hussain <daoudhussain302@gmail.com>
2024-08-28 16:41:25 +0500

    Fixed test cases

--- app/counter.test.tsx
+++ app/counter.test.tsx
@@ -8,6 +8,5 @@ it("App Router: Works with Client Components (React State)", () => {
   render(<Counter />);
   expect(screen.getByRole("heading")).toHaveTextContent("0");
   fireEvent.click(screen.getByRole("button"));
-  //Delibrately failing test cases
-  expect(screen.getByRole("heading")).toHaveTextContent("111");
+  expect(screen.getByRole("heading")).toHaveTextContent("1");
 });
